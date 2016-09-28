c22469a630127359a2159c31be21f6b6d86459de
GoogleFrog <googl3frog@gmail.com>
2016-09-28 21:24:50 +1000

    Fixed startboxes. CAI doesn't use them correctly though.

--- games/caiFight_2016_09.sdd/modinfo.lua
+++ games/caiFight_2016_09.sdd/modinfo.lua
@@ -1,5 +1,5 @@
 local modinfo = {
-  name          =	[[CAI Fight]],
+  name          =	[[CAI Fight 1016 09]],
   description   =	[[Mission Mutator]],
   modtype       =	[[1]],
   depend = {'Zero-K v1.4.9.3'},
