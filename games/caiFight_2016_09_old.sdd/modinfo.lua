a29fa5e89e1a96a073a3a6ea39a2c66a0b34425c
GoogleFrog <googl3frog@gmail.com>
2016-09-28 21:14:34 +1000

    Added 2016 09 old benchmark. This uses an engine version known to work with Spring 100.0 but otherwise has the same camera settings as 2016 09.

---
+++ games/caiFight_2016_09_old.sdd/modinfo.lua
@@ -0,0 +1,7 @@
+local modinfo = {
+  name          =	[[CAI Fight 1016 09 Old]],
+  description   =	[[Mission Mutator]],
+  modtype       =	[[1]],
+  depend = {'Zero-K test-10907-20b0bfa'},
+}
+return modinfo
