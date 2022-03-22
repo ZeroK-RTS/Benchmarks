42a9fb689214da33b376a3fe049bfcaa9818fc6c
GoogleFrog <googl3frog@gmail.com>
2022-03-22 17:25:06 +1100

    Added current benchmark files.

---
+++ games/1_10_1_3/modinfo.lua
@@ -0,0 +1,13 @@
+return {
+	name = 'Zero-K Benchmark',
+	description = 'Zero-K Benchmark',
+	shortname = 'zk',
+	version = 'v1.10.1.3',
+	mutator = '1',
+	game = '',
+	shortGame = '',
+	modtype = 1,
+	depend = {
+		'Zero-K v1.10.1.3',
+	},
+}
